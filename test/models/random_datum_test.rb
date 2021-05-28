ea6cd9a7c88e6a56c029a3174e396e5391924574
Ibrahim Awwal <ibrahim@gradescope.com>
2021-05-28 12:09:47 -0700

    Add test for random field default value

--- test/models/random_datum_test.rb
+++ test/models/random_datum_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class RandomDatumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creating a RandomDatum sets the random_seed field" do
+    rd = RandomDatum.create!
+    assert rd.random_seed.present?
+  end
 end
